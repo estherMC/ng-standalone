9a1d1668b95ffcaacc8a8e4f6cac68474561be42
Esther Martorell <esther_3@hotmail.com>
2023-08-07 14:53:53 +0200

    :lipstick: reverse alternative declaration of globl service

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { AnalyticsService } from './app/shared/analytics.service';
+//import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
@@ -13,7 +13,7 @@ if (environment.production) {
 
 //The next function add by default the BrowserModule
 bootstrapApplication(AppComponent, {
-  providers: [AnalyticsService]
+  providers: []//AnalyticsService]
 });
 // platformBrowserDynamic().bootstrapModule(AppModule)
   // .catch(err => console.error(err));
