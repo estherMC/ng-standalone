b4f72a86d2d52a439faba46e35c2f6a4248d1d31
Esther Martorell <esther_3@hotmail.com>
2023-08-07 14:35:07 +0200

    :sparkles: migrate to standalone component

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
+  standalone: true,
+  imports: [SharedModule], //necessari per afegir el mòdul que conté la directiva que subratlla
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
